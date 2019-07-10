8095cc14ff30eec0da62f2eb2f87798797c0e54d
Vishakha <40317353+Vishakha2697@users.noreply.github.com>
2019-07-10 12:36:06 -0700

    appbar done

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,27 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.blue,
         appBar: AppBar(
-          title: Text('Magic Ball'),
+          backgroundColor: Colors.teal.shade900,
+          title: Text('Ask Me Anything'),
         ),
+        body: Ball(),
       ),
     );
   }
 }
+
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
